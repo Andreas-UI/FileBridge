41ce458cc023001a2481e206e658dc3d445dfb45
unknown <andreas3739@gmail.com>
2025-01-28 15:02:46 +0800

    added scan qr section UI, added camera capabilities to capture qr code, added camera permission grant

--- app/(tabs)/(scan)/index.tsx
+++ app/(tabs)/(scan)/index.tsx
@@ -1,15 +1,83 @@
-import { Text, View } from "react-native";
+import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Text } from "@/components/ui/text";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Center } from "@/components/ui/center";
 
 export default function Index() {
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <Center className="h-full" >
+        <Text style={styles.message}>
+          We need your permission to show the camera
+        </Text>
+        <Button variant="solid" onPress={requestPermission}>
+          <ButtonText>Allow Camera Permission</ButtonText>
+        </Button>
+      </Center>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Scan Screen</Text>
+    <View style={styles.container}>
+      <CameraView
+        style={styles.camera}
+        facing={facing}
+        barcodeScannerSettings={{
+          barcodeTypes: ["qr"],
+        }}
+        onBarcodeScanned={(props) => console.log(props)}
+      >
+        <View style={styles.buttonContainer}>
+          <Button style={styles.button}>
+            <ButtonText>Upload QR</ButtonText>
+          </Button>
+        </View>
+      </CameraView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,14 +4,17 @@ import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
     <GluestackUIProvider mode="light">
       <Provider store={store}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
+        <GestureHandlerRootView>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </GestureHandlerRootView>
       </Provider>
     </GluestackUIProvider>
   );
